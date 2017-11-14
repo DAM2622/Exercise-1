fb59d8d774bf54070ca38bfc2076193e01094bbc
DAM2622 <33656752+DAM2622@users.noreply.github.com>
2017-11-14 11:44:16 +0000

    Exercise 2
    Exercise 2 for QA Consulting

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+/*
+This is the first line of a comment
+This is the second line of a comment
+//line comment 
+ */
+    public static void main(String[] args) {
+	System.out.println("Hello World One") ; // This is a comment
+	System.out.println(/*"Hello World Two"*/"This is a new line of text") ;
+	//System.out.println("Hello World Three") ;
+    }
+}
